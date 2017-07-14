75c880a94db006f9273763d3115514bc5b065984
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2017-07-14 00:29:17 -0300

    Adicionado Pis/Cofins

---
+++ src/MotorTributarioNet/Impostos/Csosns/Componentes/BaseCalculoCofins.cs
@@ -0,0 +1,42 @@
+﻿using MotorTributarioNet.Flags;
+
+namespace MotorTributarioNet.Impostos.Csosns.Componentes
+{
+    public class BaseCalculoCofins
+    {
+        private readonly TipoDesconto _tipoDesconto;
+
+        public BaseCalculoCofins(TipoDesconto tipoDesconto)
+        {
+            _tipoDesconto = tipoDesconto;
+        }
+
+        public decimal ValorProduto { get; set; }
+
+        public decimal Frete { get; set; }
+
+        public decimal Seguro { get; set; }
+
+        public decimal OutrasDespesas { get; set; }
+
+        public decimal Desconto { get; set; }
+
+        public decimal QuantidadeProduto { get; set; }
+
+        public decimal GetValorBaseCalculoCofins()
+        {
+            var baseCalculo = ValorProduto * QuantidadeProduto + Frete + Seguro + OutrasDespesas;
+
+            if (_tipoDesconto == TipoDesconto.Condincional)
+            {
+                var baseCalculoDescontoCondicional = baseCalculo + Desconto;
+
+                return baseCalculoDescontoCondicional;
+            }
+
+            var baseCalculoDescontoIncondicional = baseCalculo - Desconto;
+
+            return baseCalculoDescontoIncondicional;
+        }
+    }
+}
