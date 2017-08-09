9741e86a6f7264bed0bb5ae93a88e1d24a7ded4f
John Vanderson <vanderson182@gmail.com>
2017-08-09 11:00:32 -0700

    Criado CST Base

---
+++ src/MotorTributarioNet/Impostos/Csts/Base/CstBase.cs
@@ -0,0 +1,24 @@
+﻿using MotorTributarioNet.Flags;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MotorTributarioNet.Impostos.Csts.Base
+{
+    public abstract class CstBase
+    {
+        public OrigemMercadoria OrigemMercadoria { get; }
+        public Cst Cst { get; protected set; }
+
+        public CstBase(OrigemMercadoria origemMercadoria = OrigemMercadoria.Nacional)
+        {
+            OrigemMercadoria = origemMercadoria;
+        }
+
+        public virtual void Calcula(ITributavel tributavel)
+        {
+            throw new ArgumentException("Não existe calculo!");
+        }
+    }
+}
