8a86878624425cbb6f947949f423a9f713c68949
John Vanderson <vanderson182@gmail.com>
2017-08-09 16:31:52 -0700

    Incluso Calculo para CST 30 e teste de calculo

---
+++ src/MotorTributarioNet/Impostos/Csts/Cst30.cs
@@ -0,0 +1,42 @@
+﻿using MotorTributarioNet.Impostos.Csts.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MotorTributarioNet.Flags;
+using MotorTributarioNet.Facade;
+
+namespace MotorTributarioNet.Impostos.Csts
+{
+    public class Cst30 : CstBase
+    {
+        public ModalidadeDeterminacaoBcIcmsSt ModalidadeDeterminacaoBcIcmsSt { get; set; }
+
+        public decimal PercentualMva { get; private set; }
+        public decimal PercentualReducaoSt { get; private set; }
+        public decimal ValorBcIcmsSt { get; private set; }
+        public decimal PercentualIcmsSt { get; private set; }
+        public decimal ValorIcmsSt { get; private set; }
+
+        public Cst30(OrigemMercadoria origemMercadoria = OrigemMercadoria.Nacional, TipoDesconto tipoDesconto = TipoDesconto.Incondicional) : base(origemMercadoria, tipoDesconto)
+        {
+            Cst = Cst.Cst30;
+        }
+
+        public override void Calcula(ITributavel tributavel)
+        {
+            PercentualMva = tributavel.PercentualMva;
+            PercentualReducaoSt = tributavel.PercentualReducaoSt;
+            PercentualIcmsSt = tributavel.PercentualIcmsSt;
+
+            var facade = new FacadeCalculadoraTributacao(tributavel, TipoDesconto);
+
+            tributavel.ValorIpi = facade.CalculaIpi().Valor;
+
+            var resultadoCalculoCofins = facade.CalculaIcmsSt();
+
+            ValorBcIcmsSt = resultadoCalculoCofins.BaseCalculoIcmsSt;
+            ValorIcmsSt = resultadoCalculoCofins.ValorIcmsSt;
+        }
+    }
+}
