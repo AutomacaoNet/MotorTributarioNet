270f553b6d6a8efa049499894c32bf15e4f555df
John Vanderson <vanderson182@gmail.com>
2017-08-10 07:15:14 -0700

    incluso cst 60

---
+++ src/MotorTributarioNet/Impostos/Csts/Cst60.cs
@@ -0,0 +1,18 @@
+﻿using MotorTributarioNet.Flags;
+using MotorTributarioNet.Impostos.Csts.Base;
+
+namespace MotorTributarioNet.Impostos.Csts
+{
+    public class Cst60 : CstBase
+    {
+        public decimal PercentualBcStRetido { get; set; }
+        public decimal ValorBcStRetido { get; set; }
+
+        public Cst60(OrigemMercadoria origemMercadoria = OrigemMercadoria.Nacional, TipoDesconto tipoDesconto = TipoDesconto.Incondicional) : base(origemMercadoria, tipoDesconto)
+        {
+            Cst = Cst.Cst60;
+        }
+
+        
+    }
+}
