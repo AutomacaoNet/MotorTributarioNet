1f50bad5259c4b52bb62cef7b89c6478d29c8452
robertorp <robertoalvespereira18@gmail.com>
2017-11-04 08:29:48 -0200

    Melhoria de código

--- src/MotorTributarioNet/Util/DecimalExt.cs
+++ src/MotorTributarioNet/Util/DecimalExt.cs
@@ -27,15 +27,16 @@ namespace MotorTributarioNet.Util
     {
         public static decimal Arredondar(this decimal valor)
         {
-            
             var valorNovo = decimal.Round(valor, 2, MidpointRounding.AwayFromZero);
             var valorNovoStr = valorNovo.ToString("F" + 2, CultureInfo.CurrentCulture);
+
             return decimal.Parse(valorNovoStr);
         }
 
         public static decimal? Arredondar(this decimal? valor)
         {
             if (valor == null) return null;
+
             return Arredondar(valor.Value);
         }
 
@@ -43,12 +44,14 @@ namespace MotorTributarioNet.Util
         {
             var novoValor = Math.Round(valor, 2, MidpointRounding.AwayFromZero);
             var novoValorStr = novoValor.ToString("F" + 2, CultureInfo.CurrentCulture);
+
             return decimal.Parse(novoValorStr);
         }
 
         public static decimal? ArredondarUP(this decimal? valor)
         {
             if (valor == null) return null;
+
             return ArredondarUP(valor.Value);
         }
     }
