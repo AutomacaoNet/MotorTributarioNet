a29c614b077c8168594d60ff6fcfb7eecd7f3124
robertorp <robertoalvespereira18@gmail.com>
2018-03-02 16:57:21 -0300

    Fazendo teste para ResultadoTributacao

---
+++ src/TestMotorTributarioNet/ResultadoTributacaoTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestCalculosTributarios.Entidade;
+
+namespace TestCalculosTributarios
+{
+    [TestClass]
+    public class ResultadoTributacaoTest
+    {
+        [TestMethod]
+        public void TestaCalculoCST00()
+        {
+            var produto = new Produto();
+        }
+        
+    }
+}
