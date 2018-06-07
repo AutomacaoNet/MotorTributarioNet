0be766f759469b146353b2a820d9457b15cad9e9
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2018-06-06 21:26:24 -0300

    adicionado mais um teste de fcpst

--- src/TestMotorTributarioNet/CalculoFcpStTest.cs
+++ src/TestMotorTributarioNet/CalculoFcpStTest.cs
@@ -24,5 +24,23 @@ namespace TestCalculosTributarios
             
             Assert.AreEqual(56.00m, resultadoCalculoIcmsSt.ValorFcpSt);
         }
+
+        [TestMethod] // https://portalspedbrasil.com.br/forum/fundo-do-combate-a-probreza-icms-normal-e-fcp-st/
+        public void TestFcpSt_Valor_2_60()
+        {
+            var produto = new Produto
+            {
+                PercentualFcpSt = 2.00m,
+                ValorProduto = 100.00m,
+                QuantidadeProduto = 1.000m,
+                PercentualMva = 30.00m
+            };
+
+            var facade = new FacadeCalculadoraTributacao(produto);
+
+            var resultadoCalculoIcmsSt = facade.CalculaFcpSt();
+
+            Assert.AreEqual(2.60m, resultadoCalculoIcmsSt.ValorFcpSt);
+        }
     }
 }
