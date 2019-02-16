a8bf70d0fc2b9ad8de3f64a7cedacfc32c974bff
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2019-02-16 10:51:15 -0200

    correção em testes de fcp os calculos mudaram com a mudança de ano o destino passa a ter 100% e a origem 0%

--- src/TestMotorTributarioNet/CalculoDifalFcpTest.cs
+++ src/TestMotorTributarioNet/CalculoDifalFcpTest.cs
@@ -34,12 +34,12 @@ namespace TestCalculosTributarios
             Assert.AreEqual(60.00m, resultadoCalculoDifal.Difal);
             Assert.AreEqual(00.00m, resultadoCalculoDifal.ValorIcmsOrigem);
             Assert.AreEqual(60.00m, resultadoCalculoDifal.ValorIcmsDestino);
-            Assert.AreEqual("Valores totais do ICMS interstadual: DIFAL da UF destino 48,00 + FCP 20,00; DIFAL da UF Origem 12,00",
+            Assert.AreEqual("Valores totais do ICMS interstadual: DIFAL da UF destino 60,00 + FCP 20,00; DIFAL da UF Origem 0,00",
                 resultadoCalculoDifal.GetObservacao(new DadosMensagemDifal(decimal.Round(resultadoCalculoDifal.Fcp, 2), 
                     decimal.Round(resultadoCalculoDifal.ValorIcmsDestino, 2),
                     decimal.Round(resultadoCalculoDifal.ValorIcmsOrigem, 2))));
 
-            Assert.AreEqual("Valores totais do ICMS interstadual: DIFAL da UF destino 48,00 + FCP 20,00; DIFAL da UF Origem 12,00",
+            Assert.AreEqual("Valores totais do ICMS interstadual: DIFAL da UF destino 60,00 + FCP 20,00; DIFAL da UF Origem 0,00",
                 ResultadoCalculoDifal.GetObservacaoDifal(new DadosMensagemDifal(decimal.Round(resultadoCalculoDifal.Fcp, 2),
                     decimal.Round(resultadoCalculoDifal.ValorIcmsDestino, 2),
                     decimal.Round(resultadoCalculoDifal.ValorIcmsOrigem, 2))));
